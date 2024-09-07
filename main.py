86c28ae25dce70a9590efe88830016ba19aab03b
farrish199 <farrish199@gmail.com>
2024-09-07 14:57:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from handlers import (
     is_user_allowed, is_user_paid, save_user_data, handle_conversion, generate_random_string, create_category, create_bill, update_config
 )
 from keyboards import get_main_keyboard, get_submenu_keyboard, get_conversion_keyboard
-from config import TOKEN as TELEGRAM_BOT_TOKEN, ADMIN_BOT_ID
+from config import TOKEN as TELEGRAM_BOT_TOKEN, ADMIN_BOT_ID, ADMIN_USER_ID, ALLOWED_USER_IDS, PAID_USER_IDS, TOYYIBPAY_SECRET_KEY
 
 # Setup logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
